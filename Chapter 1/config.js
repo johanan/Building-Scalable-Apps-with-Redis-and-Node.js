e96265db66d9daab86b98ce710a0dbfa93c3ca3f
Joshua Johanan <josh359@gmail.com>
2017-08-22 21:43:07 -0400

    This is a small update for Issue #2

--- Chapter 1/config.js
+++ Chapter 1/config.js
@@ -1,7 +1,9 @@
 var config = {
 	port: 3000,
 	secret: 'secret',
-	redisUrl: 'redis://localhost',
+	//set this to the port of your Redis install
+	//you can use Docker now for ease
+	redisUrl: 'redis://localhost:32768',
 	routes: {
 		login: '/account/login',
 		logout: '/account/logout'
